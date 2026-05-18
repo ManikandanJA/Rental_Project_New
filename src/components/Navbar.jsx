3e617850da611a855292c1396230b014442a637f
ManikandanJA <148517481+ManikandanJA@users.noreply.github.com>
2026-05-18 17:14:19 +0530

    Added About Us page

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,6 +3,7 @@ import { Link } from "react-router-dom";
 function Navbar() {
   return (
   <nav>
+    <h1>Rental-WHEELS</h1>
   <Link to="/">Home</Link>
   <Link to="/vehicles">Vehicles</Link>
   <Link to="/my-bookings">Bookings</Link>
